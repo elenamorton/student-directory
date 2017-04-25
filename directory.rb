cc770b412bf65c2fdc53016dc3f5b98d115c30f4
Elena Morton <elena.uleia@gmail.com>
2017-04-25 09:30:49 +0000

    Add students names and total number

---
+++ directory.rb
@@ -0,0 +1,20 @@
+# firstly, we print the name of the students
+puts "The students of the Villan Academy"
+puts "---------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
+# finally, we print the total number of students
+print "Overall, we have "
+# it's important that print() doesn't add new line characters
+print "11"
+puts " great students"
