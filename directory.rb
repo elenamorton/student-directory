a87c14408427a0f0408b7b4c28963fa46bae6970
Elena Morton <elena.uleia@gmail.com>
2017-04-25 15:02:37 +0000

    Add number when printing names.

--- directory.rb
+++ directory.rb
@@ -43,8 +43,8 @@ end
 
 # print student names method
 def print(students)
-    students.each do |student|
-        puts "#{student[:name]}, (#{student[:cohort]} cohort) "
+    students.each_with_index do |student, index|
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) "
     end
 end 
 
